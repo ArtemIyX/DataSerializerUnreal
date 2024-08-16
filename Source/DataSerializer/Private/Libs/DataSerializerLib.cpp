62d4a6f1290d1a61a283477fb0e1d6ef93474015
Wellsaik <artem.podorozhko@outlook.com>
2024-08-16 16:07:52 +0300

    Data serializer lib, read/write data to disk

---
+++ Source/DataSerializer/Private/Libs/DataSerializerLib.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Libs/DataSerializerLib.h"
+
+#include "Serialization/ArchiveLoadCompressedProxy.h"
+#include "Serialization/ArchiveSaveCompressedProxy.h"
+
+bool UDataSerializerLib::WriteBytesToDisk(const TArray<uint8>& InBytes, FString InPath)
+{
+	return FFileHelper::SaveArrayToFile(InBytes, *InPath);
+}
+
+bool UDataSerializerLib::WriteBytesToDiskCompressed(const TArray<uint8>& InBytes, FString InPath)
+{
+	TArray<uint8> compressedData;
+	FArchiveSaveCompressedProxy compresser(compressedData, NAME_Zlib);
+	TArray<uint8> copy = InBytes;
+	compresser << copy;
+	compresser.Flush();
+	return WriteBytesToDisk(compressedData, InPath);
+}
+
+bool UDataSerializerLib::ReadBytesFromDisk(TArray<uint8>& OutBytes, FString InPath)
+{
+	return FFileHelper::LoadFileToArray(OutBytes, *InPath);
+}
+
+bool UDataSerializerLib::ReadCompressedBytesFromDisk(TArray<uint8>& OutBytes, FString InPath)
+{
+	TArray<uint8> compressedData;
+	if (!ReadBytesFromDisk(compressedData, InPath))
+		return false;
+
+	FArchiveLoadCompressedProxy Decompressor(compressedData, NAME_Zlib);
+	if (Decompressor.GetError())
+		return false;
+
+	Decompressor << OutBytes;
+
+	if (Decompressor.GetError())
+		return false;
+	return true;
+}
---
+++ Source/DataSerializer/Public/Libs/DataSerializerLib.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "DataSerializerLib.generated.h"
+
+/**
+ * Set of functions for working with data serialization and writing them to disk
+ */
+UCLASS(Blueprintable, BlueprintType)
+class DATASERIALIZER_API UDataSerializerLib : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+#pragma region Disk
+
+public:
+	UFUNCTION(BlueprintCallable, Category="UDataSerializerLib|Disk")
+	static bool WriteBytesToDisk(const TArray<uint8>& InBytes, FString InPath);
+	
+	UFUNCTION(BlueprintCallable, Category="UDataSerializerLib|Disk")
+	static bool WriteBytesToDiskCompressed(const TArray<uint8>& InBytes, FString InPath);
+
+	UFUNCTION(BlueprintCallable, Category="UDataSerializerLib|Disk")
+	static bool ReadBytesFromDisk(TArray<uint8>& OutBytes, FString InPath);
+
+	UFUNCTION(BlueprintCallable, Category="UDataSerializerLib|Disk")
+	static bool ReadCompressedBytesFromDisk(TArray<uint8>& OutBytes, FString InPath);
+#pragma endregion 
+};
