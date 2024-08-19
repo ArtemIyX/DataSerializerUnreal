ad8e79f27247d33d571af05c17ecf4bd7513dfa2
Wellsaik <artem.podorozhko@outlook.com>
2024-08-19 12:14:41 +0300

    Serialize object implementation

--- Source/DataSerializer/Private/Libs/DataSerializerLib.cpp
+++ Source/DataSerializer/Private/Libs/DataSerializerLib.cpp
@@ -5,6 +5,34 @@
 
 #include "Serialization/ArchiveLoadCompressedProxy.h"
 #include "Serialization/ArchiveSaveCompressedProxy.h"
+#include "Serialization/ObjectAndNameAsStringProxyArchive.h"
+
+FSerializationHeader::FSerializationHeader()
+{
+}
+
+FSerializationHeader::FSerializationHeader(TSubclassOf<UObject> ObjectType) :
+	GameClassName(ObjectType->GetPathName())
+{
+}
+
+void FSerializationHeader::Empty()
+{
+	GameClassName.Empty();
+}
+
+void FSerializationHeader::Read(FMemoryReader& MemoryReader)
+{
+	Empty();
+	// Get the class name
+	MemoryReader << GameClassName;
+}
+
+void FSerializationHeader::Write(FMemoryWriter& MemoryWriter)
+{
+	// Write the class name, so we know what class to load to
+	MemoryWriter << GameClassName;
+}
 
 bool UDataSerializerLib::WriteBytesToDisk(const TArray<uint8>& InBytes, FString InPath)
 {
@@ -13,11 +41,14 @@ bool UDataSerializerLib::WriteBytesToDisk(const TArray<uint8>& InBytes, FString 
 
 bool UDataSerializerLib::WriteBytesToDiskCompressed(const TArray<uint8>& InBytes, FString InPath)
 {
+	ensure(InBytes.Num() > 0);
 	TArray<uint8> compressedData;
-	FArchiveSaveCompressedProxy compresser(compressedData, NAME_Zlib);
-	TArray<uint8> copy = InBytes;
-	compresser << copy;
-	compresser.Flush();
+	{
+		FArchiveSaveCompressedProxy compresser(compressedData, NAME_Zlib);
+		TArray<uint8> copy = InBytes;
+		compresser << copy;
+		compresser.Flush();
+	}
 	return WriteBytesToDisk(compressedData, InPath);
 }
 
@@ -41,4 +72,127 @@ bool UDataSerializerLib::ReadCompressedBytesFromDisk(TArray<uint8>& OutBytes, FS
 	if (Decompressor.GetError())
 		return false;
 	return true;
+}
+
+bool UDataSerializerLib::SerializeObject(TArray<uint8>& OutBytes, UObject* InObject)
+{
+	ensure(IsValid(InObject));
+	OutBytes.Empty();
+	FMemoryWriter writer(OutBytes, true);
+
+	FSerializationHeader header(InObject->GetClass());
+	header.Write(writer);
+
+	// Then save the object state, replacing object refs and names with strings
+	FObjectAndNameAsStringProxyArchive archive(writer, false);
+	InObject->Serialize(archive);
+
+	if (archive.GetError())
+		return false;
+
+	return true;
+}
+
+bool UDataSerializerLib::DeserializeObject(const TArray<uint8>& InBytes, UObject* ObjectOuter, UObject*& OutObject)
+{
+	ensure(IsValid(ObjectOuter));
+	ensure(InBytes.Num() > 0);
+	OutObject = nullptr;
+	FMemoryReader reader(InBytes, true);
+
+	FSerializationHeader header;
+	header.Read(reader);
+
+	// Try and find it, and failing that, load it
+	UClass* gameClass = FindObject<UClass>(ANY_PACKAGE, *header.GameClassName);
+	if (gameClass == nullptr)
+	{
+		gameClass = LoadObject<UClass>(nullptr, *header.GameClassName);
+	}
+
+	// If we have a class, try and load it.
+	if (gameClass != nullptr)
+	{
+		OutObject = NewObject<UObject>(ObjectOuter, gameClass);
+
+		FObjectAndNameAsStringProxyArchive Ar(reader, true);
+		OutObject->Serialize(Ar);
+	}
+
+	return IsValid(OutObject);
+}
+
+bool UDataSerializerLib::SerializeObjects(TArray<uint8>& OutBytes, TArray<UObject*> InObjects)
+{
+	ensure(InObjects.Num() > 0);
+	OutBytes.Empty();
+	FMemoryWriter writer(OutBytes, true);
+
+	int32 n = InObjects.Num();
+	writer << n;
+	for (int32 i = 0; i < n; ++i)
+	{
+		FSerializationHeader SaveHeader(InObjects[i]->GetClass());
+		SaveHeader.Write(writer);
+
+		// Then save the object state, replacing object refs and names with strings
+		FObjectAndNameAsStringProxyArchive Ar(writer, false);
+		InObjects[i]->Serialize(Ar);
+
+		if (Ar.GetError())
+			return false;
+	}
+
+	return true;
+}
+
+bool UDataSerializerLib::DeSerializeObjects(const TArray<uint8>& InBytes, UObject* InObjectOuter, TArray<UObject*>& OutObjects)
+{
+	ensure(InBytes.Num() > 0);
+	OutObjects.Empty();	
+	FMemoryReader reader(InBytes, true);
+	
+	int32 n = 0;
+	reader << n;
+	for(int32 i = 0; i < n; ++i)
+	{
+		FSerializationHeader header;
+		header.Read(reader);
+
+		// Try and find it, and failing that, load it
+		UClass* gameClass = FindObject<UClass>(ANY_PACKAGE, *header.GameClassName);
+		if (gameClass == nullptr)
+		{
+			gameClass = LoadObject<UClass>(nullptr, *header.GameClassName);
+		}
+
+		// If we have a class, try and load it.
+		if (gameClass != nullptr)
+		{
+			UObject* resObject = nullptr;
+			resObject = NewObject<UObject>(InObjectOuter, gameClass);
+
+			FObjectAndNameAsStringProxyArchive archive(reader, true);
+			resObject->Serialize(archive);
+			OutObjects.Add(resObject);
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	if(OutObjects.Num() != n)
+	{
+		return false;
+	}
+	
+	return true;
+}
+
+TArray<uint8> UDataSerializerLib::AppendBytes(const TArray<uint8>& InLeftPart, const TArray<uint8>& InRightPart)
+{
+	TArray<uint8> arr = {InLeftPart};
+	arr.Append(InRightPart);
+	return arr;
 }
--- Source/DataSerializer/Public/Libs/DataSerializerLib.h
+++ Source/DataSerializer/Public/Libs/DataSerializerLib.h
@@ -6,6 +6,24 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "DataSerializerLib.generated.h"
 
+constexpr int32 XEUS_SAVEGAME_FILE_TYPE_TAG = 0x78657573; //XEUS
+
+USTRUCT()
+struct FSerializationHeader
+{
+	GENERATED_BODY()
+public:
+	FSerializationHeader();
+	FSerializationHeader(TSubclassOf<UObject> ObjectType);
+
+	void Empty();
+
+	void Read(FMemoryReader& MemoryReader);
+	void Write(FMemoryWriter& MemoryWriter);
+	
+	FString GameClassName;
+};
+
 /**
  * Set of functions for working with data serialization and writing them to disk
  */
@@ -30,5 +48,27 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category="UDataSerializerLib|Disk")
 	static bool ReadCompressedBytesFromDisk(TArray<uint8>& OutBytes, FString InPath);
+	
+#pragma endregion
+
+#pragma region Serialize
+	UFUNCTION(BlueprintCallable, Category="UDataSerializerLib|Serialization")
+	static bool SerializeObject(TArray<uint8>& OutBytes, UObject* InObject);
+
+	UFUNCTION(BlueprintCallable, Category="UDataSerializerLib|Serialization")
+	static bool DeserializeObject(const TArray<uint8>& InBytes, UObject* ObjectOuter, UObject*& OutObject);
+
+	/* Will add N(num) before array of bytes */
+	UFUNCTION(BlueprintCallable, Category="UDataSerializerLib|Serialization")
+	static bool SerializeObjects(TArray<uint8>& OutBytes, TArray<UObject*> InObjects);
+
+	/* Require N(num) before array of bytes */
+	UFUNCTION(BlueprintCallable, Category="UDataSerializerLib|Serialization")
+	static bool DeSerializeObjects(const TArray<uint8>& InBytes, UObject* InObjectOuter, TArray<UObject*>& OutObjects);
+#pragma endregion 
+	
+
+#pragma region Utils
+	static TArray<uint8> AppendBytes(const TArray<uint8>& InLeftPart, const TArray<uint8>& InRightPart);
 #pragma endregion 
 };
